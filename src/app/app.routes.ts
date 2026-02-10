ecb5318f6f3551f61dcc2b45fdd3d77481919af6
Francisco José Soler Conchello <franciscosoler.dev@gmail.com>
2026-02-10 22:58:51 +0100

    feat(auth): Protect library route with AuthGuard (#13)
    - Created `AuthGuard` to restrict access to specific routes based on authentication status.
    - Applied `AuthGuard` to the `/library` route in `app.routes.ts`.
    - Configured the guard to redirect unauthenticated users to the home page and display an error toast.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,16 @@ import { HomeComponent } from './home/home.component';
 import { PlatformGamesComponent } from './platform-games/platform-games.component';
 import { PlatformListComponent } from './platform-list/platform-list.component';
 import { LibraryComponent } from './library/library.component';
+import { AuthGuard } from './core/guards/auth.guard';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full' },
   { path: 'platforms', component: PlatformListComponent },
   { path: 'platforms/:id', component: PlatformGamesComponent },
-  { path: 'library', component: LibraryComponent },
+  {
+    path: 'library',
+    component: LibraryComponent,
+    canActivate: [AuthGuard]
+  },
   { path: '**', redirectTo: '' }
 ];
