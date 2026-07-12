78b6541abfca4919d570037a800754456f410987
Fran <franciscosoler.dev@gmail.com>
2026-07-12 18:41:39 +0200

    refactor: remove unused provideAnimations

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -8,7 +8,7 @@ import { provideAnimations } from '@angular/platform-browser/animations';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideHttpClient(withXhr(), 
+    provideHttpClient(withXhr(),
       withInterceptors([authInterceptor])
     ),
     provideAnimations()
